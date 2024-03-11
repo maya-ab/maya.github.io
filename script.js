c8de451102710caadfab666f2e29dbf9a7fa1e47
Mayaviri7 <66889922+Mayaviri7@users.noreply.github.com>
2024-03-11 00:43:16 -0700

    Redesign of portfolio

---
+++ script.js
@@ -0,0 +1,16 @@
+
+document.addEventListener('DOMContentLoaded', function() {
+    const buttons = document.querySelectorAll('.project-button');
+
+    buttons.forEach(button => {
+        button.addEventListener('click', function() {
+            this.classList.toggle('active');
+            const content = this.nextElementSibling;
+            if (content.style.display === "block") {
+                content.style.display = "none";
+            } else {
+                content.style.display = "block";
+            }
+        });
+    });
+});
